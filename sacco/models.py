bae180e766e2f9ea88f95c8e6212ddd3275317d2
francis <francis@gmail.com>
2024-11-14 06:55:12 +0300

    Intro to Django models

---
+++ sacco/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    weight = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'customers'
+
+
+
+
+#run the migrations
+# python manage.py makemigrations
+# python manage.py migrate
