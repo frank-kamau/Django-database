9bb33d793b7933ad3361ad13e56de5131f0079db
francis <francis@gmail.com>
2024-11-18 10:03:54 +0300

    Django Admin

--- sacco/admin.py
+++ sacco/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from sacco.models import Customer, Deposit
+
 # Register your models here.
+admin.site.site_header = 'Umoja Sacco Administration'
+admin.site.site_title = 'Sacco Administration'
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name','email','gender', 'dob']
+    search_fields = ['first_name', 'last_name','email']
+    list_filter = ['gender']
+    list_per_page = 25
+
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'created_at', 'status', 'amount']
+    search_fields = ['customer', 'status', 'amount']
+    list_per_page = 25
+    list_filter = ['status']
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Deposit, DepositAdmin)
+
+
+
+# python manage.py --help
+
+#python manage.py createsuperuser  (creating an admin in django)
+#admin@gmail.com (username)
+#123456 (password)
+
+# p@55w0dE (password)
--- sacco/models.py
+++ sacco/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.gender}"
+        return f"{self.first_name} {self.last_name} "
 
     class Meta:
         db_table = 'customers'
