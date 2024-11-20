1517e999c05d3f11c696f057dcb2058cc4b04076
francis <francis@gmail.com>
2024-11-20 12:07:02 +0300

    Adding gender radio buttons

--- sacco/app_forms.py
+++ sacco/app_forms.py
@@ -8,6 +8,7 @@ GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 
 
 class CustomerForm(forms.ModelForm):
+    gender = forms.ChoiceField(choices=GENDER_CHOICES, widget=forms.RadioSelect)
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'dob', 'weight', 'gender']
